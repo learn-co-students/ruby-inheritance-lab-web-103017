e6823fe303089a15faf7521eb3368ed59a1a4b40
eshim12 <ellishim@gmail.com>
2017-11-03 00:03:58 -0400

    complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    # @first_name = first_name
+    # @last_name = last_name
+    # @new = []
+    @knowledge = []
+  end
+
+  def learn(str)
+    self.knowledge << str
+  end
 end
