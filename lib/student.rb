7168be0831796d9992b4b00943c4763f350de337
crutell331 <tcw331@gmail.com>
2017-11-03 08:10:08 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+class Student  < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn (knowledge)
+    @knowledge << knowledge
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
